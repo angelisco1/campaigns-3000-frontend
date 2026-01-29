cfb6cb703746c6282f2e1c9405459eda5427c6db
angelvil92 <angelvil92@gmail.com>
2026-01-29 18:59:07 +0100

    changes

--- test/specs/pruebas.e2e.ts
+++ test/specs/pruebas.e2e.ts
@@ -1,15 +1,17 @@
-describe('Unos tests', () => {
-  it('Test 1', async () => {
+describe('Login: validación de campo usuario', () => {
+  it('Permite escribir el usuario en el input de login', async () => {
     await browser.url('http://localhost:4200')
-    const x = await $('/html/body/app-root/app-login/div/div/form/div[1]/input')
-    await x.setValue('usuario')
-    await expect(x).toHaveValue('usuario')
+    const usernameInput = await $('input[formcontrolname="username"]');
+    await browser.pause(4000)
+    await usernameInput.setValue('usuario')
+    await expect(usernameInput).toHaveValue('usuario')
   })
 
   it('Test 2', async () => {
     await browser.url('http://localhost:4200')
-    const x = await $('.login-container > div > form > div > input.form-input')
-    await x.setValue('usuario')
-    await expect(x).toHaveValue('usuario')
+    const simboloMoneda = await $('.login-container > div > form > div > input.form-input')
+    await browser.pause(4000)
+    await simboloMoneda.setValue('usuario')
+    await expect(simboloMoneda).toHaveValue('usuario')
   })
 })
